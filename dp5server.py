3efd954c7c8c118e93f11d806e26aa642ebc3521
Nikita Borisov <git@nikita.ca>
2013-08-23 12:46:51 +0100

    Read config files, support downloads

--- dp5server.py
+++ dp5server.py
@@ -7,7 +7,7 @@ import dp5
 
 
 # How to generate an RSA self-signed cert using openssl
-# 
+#
 # openssl genrsa -des3 -out server.key 1024
 # openssl req -new -key server.key -out server.csr
 # cp server.key server.key.org
@@ -16,20 +16,25 @@ import dp5
 
 class RootServer:
 
-    def __init__(self, is_register=True, is_lookup=True):
+    def __init__(self, config):
         self.epoch = None
 
-        self.is_register = is_register
+        self.config = config
+
+        self.is_register = config["isRegServer"]
         self.register_handlers = {}
 
-        self.is_lookup = is_lookup        
+        self.is_lookup = config["isLookupServer"]
         self.lookup_handlers = {}
 
         # For debugging
         self._add = 0
 
         self.check_epoch()
-        
+
+
+    def filenames(self, epoch):
+        return ("%s/meta%d.dat" % (self.config["datadir"], epoch), "%s/data%d.dat" % (self.config["datadir"], epoch))
 
     def getepoch(self, add=0):
         self._add += add
@@ -39,23 +44,23 @@ class RootServer:
         if self.is_lookup:
             assert self.epoch not in self.lookup_handlers
             ## TODO: If local files are not available
-            ##        Retrieve them from the registration 
+            ##        Retrieve them from the registration
             ##        server using HTTPs.
             try:
-                metafile = "datadir/meta%d.dat" % (self.epoch)
-                datafile = "datadir/data%d.dat" % (self.epoch)
+                metafile, datafile = self.filenames(self.epoch)
 
                 ## Throw exception if not found
                 open(metafile)
                 open(datafile)
 
                 server = dp5.getnewserver()
+
                 dp5.serverinitlookup(server, self.epoch, metafile, datafile)
                 self.lookup_handlers[self.epoch] = server
             except:
                 print "No metadatafile available"
                 pass
-    
+
             ## TODO: Delete old instances of the server
 
 
@@ -68,11 +73,11 @@ class RootServer:
             if self.is_register:
                 ## Initialize a new registration server
                 server = dp5.getnewserver()
-                dp5.serverinitreg(server, self.epoch, "regdir", "datadir")
+                dp5.serverinitreg(server, self.epoch, self.config["regdir"], self.config["datadir"])
                 self.register_handlers[self.epoch] = server
 
             self.update_lookup()
-            
+
         elif self.epoch < self.getepoch():
 
             ## Move epoch and initialize
@@ -81,8 +86,7 @@ class RootServer:
                 server = self.register_handlers[self.epoch]
 
                 ## Save DB and update epoch
-                meta_name = "datadir/meta%d.dat" % (self.epoch+1)
-                data_name = "datadir/data%d.dat" % (self.epoch+1)
+                meta_name, data_name = self.filenames(self.epoch+1)
                 self.epoch = dp5.serverepochchange(server, meta_name, data_name)
                 self.register_handlers[self.epoch] = server
             else:
@@ -115,7 +119,7 @@ class RootServer:
     @cherrypy.expose
     def register(self, epoch):
         "Register a number of friends"
-        ## First check if we are a registration server in a valid state        
+        ## First check if we are a registration server in a valid state
         try:
             self.check_epoch()
             assert self.epoch == int(epoch)
@@ -129,8 +133,8 @@ class RootServer:
         server = self.register_handlers[self.epoch]
 
         post_body = cherrypy.request.body.read()
-        reply_msg = dp5.serverclientreg(server, post_body)        
-        
+        reply_msg = dp5.serverclientreg(server, post_body)
+
         ## Reply with the raw data
         cherrypy.response.headers["Content-Type"] = "application/octet-stream"
         return reply_msg
@@ -148,21 +152,32 @@ class RootServer:
         server = self.lookup_handlers[self.epoch]
 
         post_body = cherrypy.request.body.read()
-        reply_msg = dp5.serverprocessrequest(server, post_body)        
-        
+        reply_msg = dp5.serverprocessrequest(server, post_body)
+
         ## Reply with the raw data
         cherrypy.response.headers["Content-Type"] = "application/octet-stream"
         return reply_msg
 
+    @cherrypy.expose
+    def download(self, epoch, metadata=False):
+        try:
+            metaname, dataname = self.filenames(int(epoch))
+            if metadata:
+                f = open(metaname)
+            else:
+                f = open(dataname)
+            cherrypy.response.headers["Content-Type"] = "application/octet-stream"
+            return f.read()
+        except:
+            raise cherrypy.HTTPError(403)
+
+def fromUnicode(x):
+    if type(x) == unicode:
+        return str(x)
+    else:
+        return x
+
 if __name__ == '__main__':
-    server_config = {
-        'server.socket_host' : '0.0.0.0',
-        'server.socket_port' : 443,
-
-        'server.ssl_module' : 'pyopenssl',
-        'server.ssl_certificate' : 'testcerts/server.crt',
-        'server.ssl_private_key' : 'testcerts/server.key'
-    }
-
-    cherrypy.config.update(server_config)
-    cherrypy.quickstart(RootServer())
+    config = json.load(file(sys.argv[1]))
+    cherrypy.config.update(dict(map(fromUnicode,x) for x in config["server"].items()))
+    cherrypy.quickstart(RootServer(config))
