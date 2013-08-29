2ffbd83f5c52716813012198c6b838b08032158f
George Danezis <georged@gmail.com>
2013-08-29 22:16:01 +0100

    Added a twisted WSGI server

---
+++ dp5twistedserver.py
@@ -0,0 +1,31 @@
+import sys
+sys.stdout = sys.stderr
+
+import atexit
+import threading
+import cherrypy
+import json
+
+from dp5server import RootServer, fromUnicode
+
+from twisted.web.wsgi import WSGIResource
+from twisted.web.server import Site
+from twisted.internet import ssl, reactor
+
+cherrypy.config.update({'environment': 'embedded'})
+
+if cherrypy.__version__.startswith('3.0') and cherrypy.engine.state == 0:
+    cherrypy.engine.start(blocking=False)
+    atexit.register(cherrypy.engine.stop)
+
+config = json.load(file('regserver.cfg'))
+cherrypy.config.update(dict(map(fromUnicode,x) for x in config["server"].items()))
+application = cherrypy.Application(RootServer(config), script_name=None, config=None)
+
+
+resource = WSGIResource(reactor, reactor.getThreadPool(), application)
+reactor.listenSSL(8443, Site(resource),ssl.DefaultOpenSSLContextFactory(
+            'testcerts/server.key', 'testcerts/server.crt'))
+
+if __name__ == "__main__":
+    reactor.run()
