f0936d1664c3745cb36912d792946fa946e647bf
George Danezis <georged@gmail.com>
2013-08-29 22:19:50 +0100

    Use the standard config file

--- dp5twistedserver.py
+++ dp5twistedserver.py
@@ -18,7 +18,7 @@ if cherrypy.__version__.startswith('3.0') and cherrypy.engine.state == 0:
     cherrypy.engine.start(blocking=False)
     atexit.register(cherrypy.engine.stop)
 
-config = json.load(file('regserver.cfg'))
+config = json.load(file(sys.argv[1]))
 cherrypy.config.update(dict(map(fromUnicode,x) for x in config["server"].items()))
 application = cherrypy.Application(RootServer(config), script_name=None, config=None)
 
