7e1ca124e32b1c4bae4d2c54e3ffb3729dfcf285
Nikita Borisov <git@nikita.ca>
2013-09-25 21:50:03 +0100

    Reg client works with refactoring

--- dp5regclient.cpp
+++ dp5regclient.cpp
@@ -9,8 +9,10 @@
 #include <algorithm>
 #include "dp5regclient.h"
 
+using namespace std;
+
 // Initialize the client by storing its private key
-DP5RegClient::DP5RegClient(const unsigned char privkey[PRIVKEY_BYTES]){
+DP5RegClient::DP5RegClient(const DP5Metadata & md, const unsigned char privkey[PRIVKEY_BYTES]) : DP5Metadata(md) {
     memmove(this->_privkey,privkey, PRIVKEY_BYTES);
 }
 
@@ -26,12 +28,11 @@ int DP5RegClient::start_reg(string &msgtosend, unsigned int next_epoch, const ve
     // Determine the target epoch for the registration
     // as the next epoch, and convert to bytes.
     
-    unsigned char epoch_bytes[EPOCH_BYTES];
+    char epoch_bytes[EPOCH_BYTES];
     epoch_num_to_bytes(epoch_bytes, next_epoch);
 
     // Placeholder for the output message
-    size_t record_length = SHAREDKEY_BYTES+DATAENC_BYTES;
-    unsigned char out_data[MAX_BUDDIES][record_length];
+    size_t record_length = SHAREDKEY_BYTES+dataenc_bytes;
     vector<string> to_sort;
 
     // For each real buddy
@@ -45,31 +46,27 @@ int DP5RegClient::start_reg(string &msgtosend, unsigned int next_epoch, const ve
         diffie_hellman(shared_dh_secret, _privkey, current_buddy.pubkey);
 
         // Derive the epoch keys 
-        unsigned char* shared_key = out_data[i];
+        unsigned char shared_key[SHAREDKEY_BYTES];
         unsigned char data_key[DATAKEY_BYTES];
-        H1H2(shared_key, data_key, epoch_bytes, mypub, shared_dh_secret);
+        H1H2(shared_key, data_key, epoch, mypub, shared_dh_secret);
 
-        // Encrypt the associated data
-        unsigned char* ciphertext = out_data[i] + (SHAREDKEY_BYTES);
-        Enc(ciphertext, data_key, current_buddy.data);
-        s.assign((char*) out_data[i], record_length);
+        s.assign((char *) shared_key, SHAREDKEY_BYTES);
+
+        s += Enc(data_key, current_buddy.data);
         to_sort.push_back(s);        
     }
     
     // Now pad the end of the array with random entries
     for(unsigned int i = buddies.size(); i < MAX_BUDDIES; i++)
     {
-        string s;
-        unsigned char* shared_key = out_data[i];
-        unsigned char* ciphertext = out_data[i] + SHAREDKEY_BYTES;
-        random_bytes(shared_key, SHAREDKEY_BYTES);
-        random_bytes(ciphertext, DATAENC_BYTES);
-        s.assign((char*)out_data[i], record_length);
+        char record[record_length];
+        random_bytes((unsigned char *) record, record_length);
+        string s(record, record_length);
         to_sort.push_back(s);
     }    
 
     // Sort the records and construct the message
-    msgtosend.assign((char *)epoch_bytes, EPOCH_BYTES);
+    msgtosend.assign(epoch_bytes, EPOCH_BYTES);
     sort(to_sort.begin(), to_sort.end());
     for (unsigned int i = 0 ; i < MAX_BUDDIES; i++){
        msgtosend += to_sort[i]; 
@@ -84,7 +81,7 @@ int DP5RegClient::complete_reg(const string &replymsg, unsigned int next_epoch){
         return 0xFE; // Meaning "wrong input length"
     
     // Parse the message
-    unsigned char * buffer = (unsigned char *) replymsg.c_str();
+    const char * buffer = replymsg.c_str();
     unsigned char server_err = buffer[0];
     unsigned int server_epoch = epoch_bytes_to_num(buffer + 1);
     
@@ -117,54 +114,53 @@ static void dump(const char *prefix, const unsigned char *data,
 
 int main(int argc, char **argv)
 {
-    DP5Params dp5;
+    DP5Metadata dp5;
     
     // Make up a client
     unsigned char client_privkey[dp5.PRIVKEY_BYTES];
     unsigned char client_pubkey[dp5.PUBKEY_BYTES];
     dp5.genkeypair(client_pubkey, client_privkey);
+    dp5.dataenc_bytes = 16;
+    dp5.epoch_len = 1800;
+    dp5.usePairings = false;
 
-    DP5RegClient client = DP5RegClient(client_privkey);
+    DP5RegClient client = DP5RegClient(dp5, client_privkey);
 
     // Make up some friends
-    vector<BuddyInfo> buds;
-    BuddyInfo buddies[10];
-    memset(buddies, 0, sizeof(BuddyInfo)*10);
+    vector<BuddyInfo> buddies(10);
     
-
     for(unsigned int i = 0; i < 10; i++){
         unsigned char bud_privkey[dp5.PRIVKEY_BYTES];
         unsigned char* bud_pubkey = buddies[i].pubkey;
         dp5.genkeypair(bud_pubkey, bud_privkey);
-        unsigned char* plain1 = buddies[i].data;
 
-        for (unsigned int j=0; j<dp5.DATAPLAIN_BYTES; ++j) {
-	        plain1[j] = 0x00 + j;
+        for (unsigned int j=0; j<dp5.dataenc_bytes; ++j) {
+            buddies[i].data.push_back(0x00 + j);
         }
-
-        buds.push_back(buddies[i]);
+        printf("Buddy %d PK: ", i);
+        dump(NULL, buddies[i].pubkey, dp5.PUBKEY_BYTES);
+        printf("Buddy %d data: ", i);
+        dump(NULL, (unsigned char  *) buddies[i].data.c_str(), dp5.dataenc_bytes);
     }
 
-    dump("Buddies ", (unsigned char *) buddies, sizeof(BuddyInfo)*10);
-
     // Make the first message
-    unsigned int next_epoch = client.current_epoch() + 1;
+    unsigned int next_epoch = dp5.current_epoch() + 1;
 
     string s;
     dump("In string ", (unsigned char *) s.c_str(), s.length());
 
-    int err1 = client.start_reg(s, next_epoch, buds);
+    int err1 = client.start_reg(s, next_epoch, buddies);
     printf("Result 1 ok: %s\n", (err1==0x00)?("True"):("False"));    
 
     dump("Out string ", (unsigned char *) s.c_str(), s.length());
 
 
-    unsigned char rmsg[1+dp5.EPOCH_BYTES];
+    char rmsg[1+dp5.EPOCH_BYTES];
     rmsg[0] = 0x00;
     // unsigned int next_epoch = dp5.current_epoch() + 1;
     dp5.epoch_num_to_bytes(1+rmsg, next_epoch);
     string rstr;
-    rstr.assign((char*)rmsg, 1+dp5.EPOCH_BYTES);
+    rstr.assign(rmsg, 1+dp5.EPOCH_BYTES);
 
     int err2 = client.complete_reg(rstr, next_epoch);
     printf("Result 2 ok: %s\n", (err2==0x00)?("True"):("False"));
