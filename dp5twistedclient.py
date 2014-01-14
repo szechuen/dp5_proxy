6e2f0e71772329becb6fff2889fb8f85dcc0b907
George D <georged@gmail.com>
2014-01-14 16:59:17 +0000

    Tweeks to python client

---
+++ dp5twistedclient.py
@@ -0,0 +1,193 @@
+from dp5asyncclient import *
+from dp5cffi import *
+
+from pprint import pformat
+
+from twisted.internet import reactor
+from twisted.internet import task
+from twisted.internet.defer import Deferred
+from twisted.internet.protocol import Protocol, connectionDone
+from twisted.web.client import Agent, ResponseDone
+from twisted.web.http_headers import Headers
+from twisted.web.client import FileBodyProducer
+
+from StringIO import StringIO
+import traceback
+import sys
+import json
+import copy
+
+from users import User
+import cPickle
+
+class BufferedReception(Protocol):
+    def __init__(self, finished):
+        self.finished = finished
+        self.bytes = None
+
+    def dataReceived(self, bytes):
+        
+        if self.bytes == None:
+            self.bytes = StringIO()
+        self.bytes.write(bytes)
+
+
+    def connectionLost(self, reason):
+        if reason.type == ResponseDone and self.bytes != None:
+            data = self.bytes.getvalue()
+            self.finished.callback(data)
+        else:
+            self.finished.errback(reason)
+
+def dp5twistedclientFactory(state):
+    ## Build an async client
+    cli = AsyncDP5Client(state)
+    cli.agent = Agent(reactor)
+
+    ## Define the networking for registration
+    def send_registration(cli, epoch, combined, msg, cb, fail):
+        if combined:
+            try:
+                ser = cli.state["combined"]["regServer"]
+            except:
+                print "Using dummy registration server: localhost:8444"
+                ser = "localhost:8444"
+            surl = str("https://"+ser+"/register?epoch=%s" % (epoch-1))
+        else:
+            try:
+                ser = cli.state["standard"]["regServer"]
+            except:
+                print "Using dummy registration server: localhost:8443"
+                ser = "localhost:8443"
+            surl = str("https://" + ser + "/register?epoch=%s" % (epoch-1))
+
+        #print "Send registration: %s" % surl
+
+        body = FileBodyProducer(StringIO(msg))
+
+        d = cli.agent.request(
+            'POST',
+            surl,
+            Headers({'User-Agent': ['DP5 Twisted Client']}),
+            body)
+
+        def err(*args):
+            print "REG ERROR", args
+            fail()
+
+        def cbRequest(response):
+            #print 'Response version:', response.version
+            #print 'Response code:', response.code
+            #print 'Response phrase:', response.phrase
+            #print 'Response headers:'
+            #print pformat(list(response.headers.getAllRawHeaders()))
+            finished = Deferred()
+            finished.addCallback(cb)
+            finished.addErrback(err)
+            response.deliverBody(BufferedReception(finished))
+            return finished
+        d.addCallback(cbRequest)
+        d.addErrback(err)
+    cli.register_handlers += [send_registration]
+
+    ## Define the networking for lookups
+    def send_lookup(cli, epoch, combined, seq, msg, cb, fail):
+        if msg == "":
+            #print "No need to relay lookup"
+            return cb("")
+
+        if combined:
+            try:
+                ser = cli.state["combined"]["lookupServers"][seq]
+            except:
+                print "Using dummy lookup server: localhost:8454"
+                ser = "localhost:8454"
+            surl = str("https://"+ser+"/lookup?epoch=%s" % epoch)
+        else:
+            try:
+                ser = cli.state["standard"]["lookupServers"][seq]
+            except:
+                print "Using dummy lookup server: localhost:8453"
+                ser = "localhost:8453"
+            surl = str("https://" + ser + "/lookup?epoch=%s" % epoch)
+
+        body = FileBodyProducer(StringIO(msg))
+
+        d = cli.agent.request(
+            'POST',
+            surl,
+            Headers({'User-Agent': ['DP5 Twisted Client']}),
+            body)
+
+        def err(*args):
+            print "LOOKUP ERROR", args
+            try:
+                args[0].raiseException()
+            except:
+                traceback.print_exc()
+
+            fail()
+
+        def cbRequest(response):
+            #print 'Response version:', response.version
+            #print 'Response code:', response.code
+            #print 'Response phrase:', response.phrase
+            #print 'Response headers:'
+            #print pformat(list(response.headers.getAllRawHeaders()))
+            finished = Deferred()
+            finished.addCallback(cb)
+            finished.addErrback(err)
+            response.deliverBody(BufferedReception(finished))
+            return finished
+        d.addCallback(cbRequest)
+        d.addErrback(err)
+    cli.lookup_handlers += [send_lookup]
+
+    def loopupdate():
+        # print ".",
+        cli.update()
+
+    cli.l = task.LoopingCall(loopupdate)
+    period = float(cli.state["epoch_lengthCB"] / 4.0)
+    print "Update every %2.2f secs" % period
+    cli.l.start(period) # call every second
+
+    return cli
+
+
+if __name__ == "__main__":
+    try:
+        config = json.load(file(sys.argv[1]))
+        print "Loading config from file \"%s\"" % sys.argv[1]
+    except Exception as e:
+        traceback.print_exc()
+        config = {}
+        print "No configuration file"
+        sys.exit(1)
+
+    ## Now load a standard file of users
+    try:
+        uxs = cPickle.load(file(sys.argv[2]))
+    except Exception as e:
+        print "No users file specified"
+        print e
+        sys.exit(1)
+
+    def handler(state, event):
+            print state["Name"], event
+
+    clients = []
+    for x, u in enumerate(uxs):
+        state = copy.deepcopy(config)
+        state["Name"] = ("Client%s"%x)
+        state["ltID"] = u.pub + u.priv
+
+        xcli = dp5twistedclientFactory(state)
+        for i,f in enumerate(u.buddies):
+            xcli.set_friend(f, "F%s"%i)
+
+        xcli.set_event_handler(handler)
+        
+        clients += [xcli]
+
+    reactor.run()
